c4bd2888112c09abbb891cc87d9440f956562ab0
Alessio.Loginov <alekseyloginov79@gmail.com>
2022-11-19 13:14:08 +0300

    Modifided

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,6 @@
 public class CreditPaymentService {
-    public static CreditPaymentOptions calculate(int creditRub, int creditYears) {
-        CreditPaymentOptions myOptions = new CreditPaymentOptions();
-        myOptions.rateYears = 9.99;
-        double rateMonth = 9.99 * 0.01 / 12; //считаем месячную ставку по кредиту
+    public static CreditPaymentOptions calculate(int creditRub, int creditYears, CreditPaymentOptions myOptions) {
+        double rateMonth = myOptions.rateYears * 0.01 / 12; //считаем месячную ставку по кредиту
         double locDouble = Math.pow(1 + rateMonth, creditYears * 12);
         locDouble = (rateMonth * locDouble) / (locDouble - 1); //шаманство с формулой аннуитетного платежа
         int payMonth = (int) ((int) creditRub * locDouble); //вычисляем ежемесячный платеж
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         CreditPaymentOptions myOptions = new CreditPaymentOptions();
         myOptions.rateYears = 9.99; //годовая процентная ставка
         CreditPaymentService service = new CreditPaymentService(); //создаем экземпляр класса
-        myOptions = CreditPaymentService.calculate(creditRub, creditYears); //вызываем метод класса
+        myOptions = CreditPaymentService.calculate(creditRub, creditYears, myOptions); //вызываем метод класса
         System.out.println("Сумма кредита: " + creditRub + " ₽");
         System.out.println("Срок кредита: " + creditYears + " год");
         System.out.println("Ежемесячный платеж: " + myOptions.payMonth + " ₽");
@@ -13,7 +13,7 @@ public class Main {
         System.out.println();
         creditRub = 1_000_000; //сумма кредита в рублях
         creditYears = 2;// срок кредита в годах
-        myOptions = CreditPaymentService.calculate(creditRub, creditYears); //вызываем метод класса
+        myOptions = CreditPaymentService.calculate(creditRub, creditYears, myOptions); //вызываем метод класса
         System.out.println("Сумма кредита: " + creditRub + " ₽");
         System.out.println("Срок кредита: " + creditYears + " года");
         System.out.println("Ежемесячный платеж: " + myOptions.payMonth + " ₽");
@@ -21,7 +21,7 @@ public class Main {
         System.out.println();
         creditRub = 1_000_000; //сумма кредита в рублях
         creditYears = 3;// срок кредита в годах
-        myOptions = CreditPaymentService.calculate(creditRub, creditYears); //вызываем метод класса
+        myOptions = CreditPaymentService.calculate(creditRub, creditYears, myOptions); //вызываем метод класса
         System.out.println("Сумма кредита: " + creditRub + " ₽");
         System.out.println("Срок кредита: " + creditYears + " года");
         System.out.println("Ежемесячный платеж: " + myOptions.payMonth + " ₽");
